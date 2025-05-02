8abd0d498c6d9bb0f469b278e223de0771895e0e
krasilnikov.orchid@gmail.com <krasilnikov.orchid@gmail.com>
2025-05-03 00:58:43 +0200

    feat: add axios signup request

--- src/lib/components/auth/SignUpForm.svelte
+++ src/lib/components/auth/SignUpForm.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
+    import axios from "axios";
+
     let email = $state('');
     let password = $state('');
 
-    const signUp = () => {
+    const signUp = async () => {
+        const signupData = {
+            email,
+            password
+        };
 
+        console.log(signupData);
+
+        const res = await axios.post('http://localhost:8080/api/auth/register', signupData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(res);
     }
 </script>
 
 <div class="loginForm">
     <input bind:value={email} type='text' placeholder="Type your email..."/>
     <input bind:value={password} type='password' placeholder="Type your password..."/>
-    <button>Sign Up</button>
+    <button onclick={signUp}>Sign Up</button>
 </div>
 
 <style>
